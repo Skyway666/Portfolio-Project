845fd0700fa5f3b47bda8b34b89c05821bc46b19
Skyway666 <lucasgarciamateu@gmail.com>
2020-11-13 00:48:43 +0100

    Basic version of the portfolio page built

--- portfolio/src/views/Home.vue
+++ portfolio/src/views/Home.vue
@@ -1,8 +1,42 @@
 <template>
-  <div>
-    <sui-button icon="heart">Click Here</sui-button>
-    <sui-button content="Content as a prop" primary inverted/>
+  <div style="
+      margin-left: 300px;
+      margin-right: 300px;"
+  >
+    <sui-header size="huge" style="fontSize: 50px"> Lucas García Mateu</sui-header>
+    <sui-segment 
+      style="fontSize: 20px; 
+      text-align: left;
+      line-height: 1.5;"
+      padded
+    >
+      Lucas es un programador versátil, capaz de aislar y resolver cualquier problema gracias a su 
+      metodologia operativa eficiente y constante. Sin embargo, su perfíl técnico no interfiere con
+      su capacidad de encontrar soluciones creativas y su sentido común a la hora de tomar acción de
+      forma proactiva. Por último, tiene buenas habilidades comunicativas y es honesto, siempre se
+      puede contar con él para hablar de cualquier cosa y nunca esconde los problemas con los que se 
+      encuentra en el desempeño de sus tareas.
+    </sui-segment>
+    <sui-header size="huge" style="fontSize: 40px"> Remarkable projects</sui-header>
+    <sui-button-group :widths="3">
+      <sui-button> <a href="https://angryfrogdev.github.io/Dungeon-Brawler/">Dungeon Brawler</a> </sui-button>
+      <sui-button> <a href="https://skyway666.github.io/Kuroko-Engine/"> Kuroko Engine </a> </sui-button>
+      <sui-button> <a href="https://rustiktie.github.io/project-3-bcn/"> Malita, Soul of a Warrior </a> </sui-button>
+    </sui-button-group>
+    <sui-divider/>
+    <sui-button-group :widths="3">
+      <sui-button> <a href="https://mtorresform.wixsite.com/apocalypsetycoonupc"> Apocalypse Tycoon </a> </sui-button>
+      <sui-button> <a href="https://dynamiccast.itch.io/asun"> Asun </a> </sui-button>
+      <sui-button> <a href="https://www.youtube.com/watch?v=y2ManVDD-fM"> Bubble Ball </a> </sui-button>
+    </sui-button-group>
+    <sui-divider/>
+    <sui-button-group :widths="3">
+      <sui-button> <a href="https://github.com/Skyway666/TFG-investigation"> Mirror </a> </sui-button>
+      <sui-button> <a href="https://github.com/Adria-F/QT-Graphics-Engine"> Advanced graphics engine </a> </sui-button>
+      <sui-button> <a href="https://drive.google.com/drive/folders/1xC-PnGSno5CikSj15lRlQjuQ_unLwy1s"> Swamp Chaos </a> </sui-button>
+    </sui-button-group>
   </div>
+  
 </template>
 
 <script>
@@ -12,6 +46,7 @@ export default {
     msg: String
   }
 }
+//@click="console.log('https://www.youtube.com/watch?v=rA4eesTbNKE');"
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
@@ -27,7 +62,5 @@ li {
   display: inline-block;
   margin: 0 10px;
 }
-a {
-  color: #42b983;
-}
 </style>
+
