3cf43853badf662a8060469fcb294f725decf25d
Skyway666 <lucasgarciamateu@gmail.com>
2020-11-20 18:04:09 +0100

    Maybe with this it works

--- portfolio/vue.config.js
+++ portfolio/vue.config.js
@@ -1,5 +1,7 @@
+// vue.config.js file to be place in the root of your repository
+
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production'
-      ? '/portfolio-project/'
-      : '/'
-  }
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/Portfolio-Project/'
+    : '/'
+}
